23a969b76f71aec34ccfe3a17b3b6f9a1c46f534
EvgenDev09 <eugene.lippa@gmail.com>
2024-02-15 18:44:17 +0300

    Add project files.

---
+++ Project41/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n = 20;
+	double d = 2.5;
+	float f = 2.5;
+	string s = "string";
+	char c = 'q';
+	bool b = true;
+	cout << sizeof(n) << endl;
+	cout << sizeof(d) << endl;
+	cout << sizeof(f) << endl;
+	cout << sizeof(s) << endl;
+	cout << sizeof(c) << endl;
+	cout << sizeof(b) << endl;
+	//
+	return 0;
+}
